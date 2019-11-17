4c54a98866b5d30592c8f9bf8ab10553b00e783f
Michelle Willcox <shellwillcox@gmail.com>
2019-11-16 16:21:58 -0800

    creating TableView component and pulling in data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import TableView from './components/Table View';
+import TableView from './components/TableView';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TableView />
+    <div className="app container">
+      <header>
+        <h1>Bay Area Commute Patterns</h1>
+        <h3>How and where do people commute in the Bay Area?</h3>
+        <p> Commute patterns, more commonly referred to as county-to-county commute flows, reflect the number of individuals traveling within and between various counties for commuting purposes. DATA SOURCE: U.S. Census Transportation Planning Package 2010</p>
       </header>
+      <TableView />
     </div>
   );
 }
